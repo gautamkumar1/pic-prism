4683741239df4a7f7f6f74d143d232ed8cfd2d6c
gautamkumar1 <gautamraaz936@gmail.com>
2024-07-02 23:23:55 +0530

    PhotoGallery and Image Components Created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../components/HeroSection"
+import PhotoGallery from "../components/PhotoGallery"
 
 
 function Home() {
   return (
     <div className="mt-36">
     <HeroSection />
+    <PhotoGallery/>
   </div>
   )
 }
